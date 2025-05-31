2a93490dff2de07584641b02eba5ae54f2677f17
Typhoon06 <wonhui0222@gmail.com>
2025-05-31 11:59:27 +0900

    Created my AI word game

---
+++ Word_Game/word_data.py
@@ -0,0 +1,43 @@
+#각 난이도 별로 단어와 뜻을 모아둔 사전(dictionary)입니다.
+#나중에 퀴즈 출제할 때 여기서 가져다 씁니다.
+#각 단어는 key로, 뜻은 value로 저장되어 있습니다.
+
+word_dict = {
+    'easy': {
+        'apple': '사과',
+        'banana': '바나나',
+        'grape': '포도',
+        'orange': '오렌지',
+        'peach': '복숭아',
+        'pear': '배',
+        'kiwi': '키위',
+        'melon': '멜론',
+        'mango': '망고',
+        'lemon': '레몬'
+    },
+    'normal': {
+        'strawberry': '딸기',
+        'blueberry': '블루베리',
+        'watermelon': '수박',
+        'pineapple': '파인애플',
+        'coconut': '코코넛',
+        'papaya': '파파야',
+        'pomegranate': '석류',
+        'raspberry': '라즈베리',
+        'blackberry': '블랙베리',
+        'tangerine': '귤'
+    },
+    # 추가 단어를 여기에 작성하세요
+    'hard': {
+        'cherry': '체리',
+        'apricot': '살구',
+        'fig': '무화과',
+        'guava': '구아바',
+        'lychee': '리치',
+        'dragonfruit': '용과',
+        'jackfruit': '잭프룻',
+        'passionfruit': '패션프루트',
+        'starfruit': '별열매',
+        'durian': '두리안'
+    }
+}
